f3f52b9b7a970540242f314ae210f3356cc86ba7
Carlos Roberto S Camilo <carlos.rs.camilo@unesp.br>
2023-03-04 13:59:13 -0300

    feat: index.html com nomes e links

--- instalar_nginx.sh
+++ instalar_nginx.sh
@@ -4,10 +4,10 @@
 
 #Se a instância já tiver sido iniciada, digite as instruções individualmente no terminal da máquina.
 #Ou copie o arquivo para a máquina e execute como um script:
-# sudo chmod +x 
+# sudo chmod +x instalar_nginx.sh
 # ./instalar_nginx.sh
 
-#Se a instância ainda será criada, copie o script para janela "Advanced details > User data" na criação da máquina.
+#Se a instância ainda será criada, copie o script para janela "Advanced details > User data" na interface de configuração da instância.
 #Neste caso, o nginx já estará disponível após a inicialização da máquina.
 
 #Instala o nginx
@@ -18,7 +18,9 @@ sudo systemctl start nginx
 
 #Configura o nginx para inicializar automaticamente com o sistema
 sudo systemctl enable nginx
-systemctl enable nginx
+
+#Altera a configuração da porta http para 9000
+sudo sed -i 's/listen       80/listen      9000/g' /etc/nginx/nginx.conf
 
 #Necessário reiniciar o nginx após fazer alterações no projeto (index.html)
 sudo systemctl restart nginx
